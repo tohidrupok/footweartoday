dbc10f89abd4dfd4004f529e03768785835e78ed
tohidrupok <pcgamertohid@gmail.com>
2024-11-29 12:37:53 +0600

    Project complate after another repo

---
+++ job/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .models import Job
+from .forms import ResumeForm
+
+def job_list(request):
+    jobs = Job.objects.exclude(title="Post Resume")
+    post_resume = Job.objects.filter(title="Post Resume")    # must be add upload resume model's title is:"Post Resume". {must use space}
+    context = {'jobs': jobs , 'post_resume':post_resume }
+    return render(request, 'job_portal/job_list.html', context)
+
+def submit_resume(request, job_id):
+    job = Job.objects.get(id=job_id)
+    if request.method == 'POST':
+        form = ResumeForm(request.POST, request.FILES)
+        if form.is_valid():
+            # Save the resume to the job
+             resume_instance = form.save(commit=False)
+             resume_instance.job = job  # Associate the resume with the job
+             resume_instance.save()
+             return render(request, 'job_portal/resume_submitted.html')
+    else:
+        form = ResumeForm()
+    return render(request, 'job_portal/submit_resume.html', {'form': form, 'job': job})  
+
+
+    # job done for this project
---
+++ vlogs/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from . models import *
+
+class BlogAdmin(admin.ModelAdmin): 
+     list_display = ['title']    
+     prepopulated_fields = {'slug' : ('title',)} 
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Contact)
+admin.site.register(Home)
+admin.site.register(Video_HomePage)
+admin.site.register(Management_Massage)
+admin.site.register(Local_Factory)
